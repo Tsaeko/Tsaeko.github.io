aa3d4d90baf6d2041048122c62ac2c6b31a023eb
Tsaeko <50843497+Tsaeko@users.noreply.github.com>
2020-03-02 16:27:29 +0100

    Create projectile.js

---
+++ projectile.js
@@ -0,0 +1,15 @@
+ class Projectile {
+    constructor(source, target) {
+      this.source = source;
+      this.pos = createVector(source.pos.x, source.pos.y);
+     
+      this.dir = target.sub(this.pos);
+    }
+    show() {
+      circle(this.pos.x, this.pos.y, 10);
+    }
+    move() {
+      this.dir.normalize();
+     this.pos.add(this.dir);
+    }
+  }
