da3beed2dd60c649b4d3bde818512565bb9c3a69
Tsaeko <50843497+Tsaeko@users.noreply.github.com>
2020-03-02 16:34:22 +0100

    Update movement.js

--- movement.js
+++ movement.js
@@ -1,14 +1,14 @@
 function keyPressed() {
   if (keyCode == 87) { //W
-    p1.pos.y -= 10;
+    p.pos.y -= 10;
   }
   if (keyCode == 83) { //s
-    p1.pos.y += 10;
+    p.pos.y += 10;
   }
   if (keyCode == 65) { //a
-    p1.pos.x -= 10;
+    p.pos.x -= 10;
   }
   if (keyCode == 68) { //d
-    p1.pos.x += 10;
+    p.pos.x += 10;
   }
 }
