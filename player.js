d5338fb508dd329e1d17de4d9294ad369be2829b
Tsaeko <gittsaeko@gmail.com>
2020-03-08 21:12:47 +0100

    included PlayerInfo

--- player.js
+++ player.js
@@ -6,6 +6,7 @@ class Player {
     this.shooting = false;
     this.hp = 100;
     this.size = 20;
+    this.info = new PlayerInfo(this);
   }
   shoot() {
     let minSpray = 1 - (this.angle / 100);
@@ -22,6 +23,24 @@ class Player {
   }
   show() {
     circle(this.pos.x, this.pos.y, this.size);
-    text("HP:" + this.hp, 40, 10);
+    this.info.show();
   }
+
+}
+
+class PlayerInfo {
+
+  constructor(player){
+    this.player=player;
+    this.pos =createVector(40,10);
+  }
+
+  show(){
+    let x= this.pos.x;
+    let y= this.pos.y;
+    let pl =this.player;
+    text("HP: "+pl.hp,x,y+10);
+    text("Projectiles: ",x,y+20);
+}
+
 }
