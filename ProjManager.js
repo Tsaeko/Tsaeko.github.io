3b5a78b9aadba16847609e3b4fbe13366168b47a
Tsaeko <gittsaeko@gmail.com>
2020-03-08 21:27:36 +0100

    discarding old Projectiles

--- ProjManager.js
+++ ProjManager.js
@@ -6,10 +6,18 @@
       this.projs.push(new Projectile(source, createVector(targetX, targetY)));
     }
     show() {
+      let discardList=[];
       for (let proj of this.projs) {
         stroke(0,50);
         proj.show();
         proj.move();
+        if(proj.pos.x < 0 || proj.pos.x>width|| proj.pos.y<0 || proj.pos.y>height){
+          discardList.push(proj);
+        }
       }
+      this.projs = this.projs.filter( function (el){
+        return !discardList.includes(el);
+      });
+
     }
   }
--- projectile.js
+++ projectile.js
@@ -1,16 +1,16 @@
  class Projectile {
-    constructor(source, target) {
-      this.source = source;
-      this.pos = createVector(source.pos.x, source.pos.y);
+   constructor(source, target) {
+     this.source = source;
+     this.pos = createVector(source.pos.x, source.pos.y);
 
-      this.dir = target.sub(this.pos);
-    }
-    show() {
+     this.dir = target.sub(this.pos);
+   }
+   show() {
 
-      circle(this.pos.x, this.pos.y, 10);
-    }
-    move() {
-      this.dir.normalize();
+     circle(this.pos.x, this.pos.y, 10);
+   }
+   move() {
+     this.dir.normalize();
      this.pos.add(this.dir);
-    }
-  }
+   }
+ }
