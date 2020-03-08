46b6fec970ca8da61717eefaeb53ef0b2921b64c
Tsaeko <gittsaeko@gmail.com>
2020-03-08 21:05:05 +0100

    spray

--- player.js
+++ player.js
@@ -3,23 +3,25 @@ class Player {
     this.pos = createVector(width / 2, height / 2);
     this.projCount = 5;
     this.angle = 40;
-    this.shooting=false;
-     this.hp = 100;   
+    this.shooting = false;
+    this.hp = 100;
     this.size = 20;
   }
   shoot() {
     let minSpray = 1 - (this.angle / 100);
     let maxSpray = 1 + (this.angle / 100);
-    
+
     for (let pCounter = 0; pCounter < this.projCount; pCounter++) {
-      let spray= random(minSpray,maxSpray);
-      pManager.add(this, mouseX*spray ,  mouseY*spray);
-      }
-    }
-    show() {
-      circle(this.pos.x, this.pos.y, this.size);
-        text("HP:" + this.hp, 40, 10);
+      let spray = random(minSpray, maxSpray);
+      let dir = createVector(mouseX, mouseY);
+      dir.mult(spray, spray);
+
+      pManager.add(this, dir.x, dir.y);
+
     }
   }
-
-
+  show() {
+    circle(this.pos.x, this.pos.y, this.size);
+    text("HP:" + this.hp, 40, 10);
+  }
+}
